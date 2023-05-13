310e7dc0e800297395750cc7962f6b058406c13e
jaydamani <jaydamani567@gmail.com>
2023-05-13 12:52:43 +0530

    fix: update field name

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('/api/hello', function (req, res) {
 
 app.get('/api/whoami', function(req, res) {
   res.json({
-    ip: req.socket.remoteAddress,
+    ipaddress: req.socket.remoteAddress,
     language: 'en-US',
     software: 'js'
   })
